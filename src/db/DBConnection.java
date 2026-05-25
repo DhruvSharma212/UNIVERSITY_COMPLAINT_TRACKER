1e46b74b7548367cca3c66ef7a690b66ae54247f
DhruvSharma212 <dhruvsharma90310@gmail.com>
2026-05-26 01:51:19 +0530

    Refactor DBConnection class formatting

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -5,8 +5,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/complaintdb";
     private static final String USER = "root";
-    private static final String PASS = "Dhruv@123"; // apna password
-
+    private static final String PASS = "Dhruv@123";
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
