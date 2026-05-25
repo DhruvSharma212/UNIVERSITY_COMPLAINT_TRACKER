807175d34c908ea91697b5abb5c66ea526265f5e
DhruvSharma212 <dhruvsharma90310@gmail.com>
2026-05-26 02:01:53 +0530

    Fix formatting and remove unnecessary lines in Session.java

--- src/util/Session.java
+++ src/util/Session.java
@@ -6,7 +6,6 @@ public class Session {
     public static String userName;
     public static String email;
 
-    // 🔥 LOGOUT METHOD
     public static void clear() {
         userId = 0;
         userName = null;
